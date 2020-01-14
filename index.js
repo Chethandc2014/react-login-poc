3fc2a75435eab66a92dca9bcb1e8355d6df158d4
Chethandc2014 <chethandc2014@gmail.com>
2020-01-14 15:09:59 +0530

    4 files modified

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
 import {Login} from './login'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.min.css';
 import './style.css';
 
 class App extends Component {
--- login.js
+++ login.js
@@ -9,12 +9,30 @@ export class Login extends React.Component{
  }
   render(){
     return(
-      <div>
-      <form>
-      UserName: <input type="text"/><br/>
-      Password: <input type="passsword"/><br/>
-      <button>Login</button>
-      </form>
+      <div className="container lgtop">
+         <div className="row">
+          <div className="col-md-4 col-xs-4 aln" >
+              <label for="usr">User Name:</label>
+          </div>
+          <div className="col-md-3 col-lg-3">
+           <input type="text" className="form-control" id="usr"/>
+          </div>
+        </div>
+          <div className="row">
+              <div className="col-md-4 col-xs-4 aln">
+                <label for="pwd">Password:</label>
+              </div>
+              <div className="col-md-6 col-lg-3">
+              <input type="password"  className="form-control"v id="pwd"/>
+              </div>
+          </div>
+          <div className="row justify-content-center">
+
+              <div >
+              <button type="button" className="btn btn-primary lgbtn">Login
+              </button>
+              </div>
+          </div>
       </div>
     );
   }
